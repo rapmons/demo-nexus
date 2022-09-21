231cc65ece89828f2e7a4196c9dc8aa65221946e
rapmons <lethibinhka123@gmail.com>
2022-09-21 11:00:51 +0700

    update

---
+++ src/main/java/com/example/demonexus1/DemoNexus1Application.java
@@ -0,0 +1,13 @@
+package com.example.demonexus1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNexus1Application {
+
+    int plus(int a, int b) {
+        return a + b;
+    }
+
+}
